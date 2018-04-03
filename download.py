04c2ab80027b25a898313689309cb5c85c2632a8
Tetsuro Mori <tetsu@users.noreply.github.com>
2018-04-03 17:49:08 +0900

    Uploaded download.py

---
+++ download.py
@@ -0,0 +1,34 @@
+# Download specified photo from Flickr 
+
+from flickrapi import FlickrAPI
+from urllib.request import urlretrieve
+from pprint import pprint
+import os, time, sys
+
+#flickr API info
+key = "6360b061367b98ceac053c516ea78ef5"
+secret = "f642d5f83066bb0b"
+waitTime = 0.5
+
+#image folders
+animalName = sys.argv[1]
+saveDir = "./" + animalName
+flickr = FlickrAPI(key, secret, format='parsed-json')
+result = flickr.photos.search(
+    text = animalName,
+    per_page = 400,
+    media = 'photos',
+    sort = 'relevance',
+    safe_search = 1,
+    extras = 'url_q, license'
+)
+
+photos = result['photos']
+# pprint(photos)
+
+for i, photo in enumerate(photos['photo']):
+    url_q = photo['url_q']
+    filepath = saveDir + '/' + photo['id'] + '.jpg'
+    if os.path.exists(filepath): continue
+    urlretrieve(url_q, filepath)
+    time.sleep(waitTime)
