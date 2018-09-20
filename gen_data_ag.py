5ecb4c5bae504d1e7c43c6d6d4773ddcc6658546
tetsu <tetsuromori@gmail.com>
2018-09-20 15:33:32 +0900

    Add image multiplication methods

---
+++ gen_data_ag.py
@@ -0,0 +1,56 @@
+from PIL import Image
+import os, glob
+import numpy as np
+from sklearn import model_selection
+from config import env
+
+classes = env.CLASSES
+num_classes = len(classes)
+image_size = env.IMAGE_SIZE
+num_test = 100
+
+#load images
+x_train = []
+x_test = []
+y_train = []
+y_test = []
+
+for index, classlabel in enumerate(classes):
+    photo_dir = "./images/" + classlabel
+    files = glob.glob(photo_dir + "/*.jpg")
+    for i, file in enumerate(files):
+        if i >= 180: break
+        image = Image.open(file)
+        image = image.convert("RGB")
+        image = image.resize((image_size, image_size))
+        data = np.asarray(image)
+
+        if i < num_test:
+            x_test.append(data)
+            y_test.append(index)
+        else:
+            # Generate more images to increase the number of images to train
+            x_train.append(data)
+            y_train.append(index)
+
+            # Multiply images by rotating
+            for angle in range(-20, 20, 5):
+                img_r = image.rotate(angle)
+                data = np.asarray(img_r)
+                x_train.append(data)
+                y_train.append(index)
+
+            # Multipy images by transposing
+            img_trans = image.transpose(Image.FLIP_LEFT_RIGHT)
+            data = np.asarray(img_trans)
+            x_train.append(data)
+            y_train.append(index)
+
+
+x_train = np.array(x_train)
+x_test = np.array(x_test)
+y_train = np.array(y_train)
+y_test = np.array(y_test)
+
+xy = (x_train, x_test, y_train, y_test)
+np.save("./image_ag.npy", xy)
---
+++ image_cnn_ag.py
@@ -0,0 +1,72 @@
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras.utils  import np_utils
+import keras
+import numpy as np
+from config import env
+
+classes = env.CLASSES
+num_classes = len(classes)
+image_size = env.IMAGE_SIZE
+
+def main():
+    x_train, x_test, y_train, y_test = np.load("./image_ag.npy")
+
+    # convert RGB integers (0-255) to floating numbers between 0 and 1.
+    x_train = x_train.astype("float") / 256
+    x_test = x_test.astype("float") / 256
+
+    # Use one-hot-vector to assing correct to 1 and incorrect data to 0
+    y_train = np_utils.to_categorical(y_train, num_classes)
+    y_test = np_utils.to_categorical(y_test, num_classes)
+
+    model = model_train(x_train, y_train)
+    model_eval(model, x_test, y_test)
+
+
+def model_train(x, y):
+    model = Sequential()
+    model.add(Conv2D(32, (3,3), padding='same', input_shape=x.shape[1:]))
+    model.add(Activation('relu'))
+    model.add(Conv2D(32, (3,3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Dropout(0.25))
+
+    model.add(Conv2D(64, (3,3), padding='same'))
+    model.add(Activation('relu'))
+    model.add(Conv2D(64, (3,3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten())
+    model.add(Dense(512))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(3))
+    model.add(Activation('softmax'))
+
+    opt = keras.optimizers.rmsprop(lr=0.0001, decay=1e-6)
+
+    model.compile(
+        loss='categorical_crossentropy',
+        optimizer=opt,
+        metrics=['accuracy']
+    )
+
+    model.fit(x, y, batch_size=32, epochs=100)
+
+    # save this model as file
+    model.save('./image_cnn_ag.h5')
+
+    return model
+
+def model_eval(model, x, y):
+    scores = model.evaluate(x, y, verbose=1)
+    print('Test Loss: ', scores[0])
+    print('Accuracy: ', scores[1])
+
+if __name__ == "__main__":
+    main()
