b3304d6fb04a6f98b85f2b8fc3fa7640c71a8949
tetsuromori <tetsuromori@gmail.com>
2018-09-25 21:32:35 +0900

    Modularize gen_data.py

--- gen_data.py
+++ gen_data.py
@@ -8,24 +8,31 @@ classes = env.CLASSES
 num_classes = len(classes)
 image_size = env.IMAGE_SIZE
 
-#load images
-x = []
-y = []
-for index, classlabel in enumerate(classes):
-    photo_dir = "./images/" + classlabel
-    files = glob.glob(photo_dir + "/*.jpg")
-    for i, file in enumerate(files):
-        if i >= 180: break
-        image = Image.open(file)
-        image = image.convert("RGB")
-        image = image.resize((image_size, image_size))
-        data = np.asarray(image)
-        x.append(data)
-        y.append(index)
+def numpify_images():
+    #load images
+    x = []
+    y = []
+    for index, classlabel in enumerate(classes):
+        photo_dir = "./images/" + classlabel
+        files = glob.glob(photo_dir + "/*.jpg")
+        for i, file in enumerate(files):
+            if i >= 180: break
+            image = Image.open(file)
+            image = image.convert("RGB")
+            image = image.resize((image_size, image_size))
+            data = np.asarray(image)
+            x.append(data)
+            y.append(index)
 
-x = np.array(x)
-y = np.array(y)
+    x = np.array(x)
+    y = np.array(y)
 
-x_train, x_test, y_train, y_test = model_selection.train_test_split(x, y)
-xy = (x_train, x_test, y_train, y_test)
-np.save("./image.npy", xy)
+    x_train, x_test, y_train, y_test = model_selection.train_test_split(x, y)
+    xy = (x_train, x_test, y_train, y_test)
+    np.save("./image.npy", xy)
+
+def main():
+    numpify_images()
+
+if __name__ == "__main__":
+    main()
